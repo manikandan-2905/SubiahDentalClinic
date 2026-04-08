c789eb9d89214c6aa235c9620a442db6cd4cc13d
Manikandan V <manikandan12905@gmail.com>
2026-04-08 12:48:48 +0530

    Update patient.js

--- backend/models/patient.js
+++ backend/models/patient.js
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-﻿const mongoose = require('mongoose');
-=======
 const mongoose = require('mongoose');
->>>>>>> a3abea51565cabad0d2f07e250f0931d98fc4613
 
 const VisitSchema = new mongoose.Schema({
   date: String,
   problem: String,
   treatment: String,
   medication: String,
-<<<<<<< HEAD
-=======
-  diagnosis: String,
-  toothNumber: String,
-  medicines: String,
->>>>>>> a3abea51565cabad0d2f07e250f0931d98fc4613
   fee: Number
 }, { _id: false });
 
@@ -29,10 +19,7 @@ const PatientSchema = new mongoose.Schema({
   category: String,
   lastVisit: String,
   pastMedicalHistory: String,
-<<<<<<< HEAD
   balance: { type: Number, default: 0 },
-=======
->>>>>>> a3abea51565cabad0d2f07e250f0931d98fc4613
   visits: [VisitSchema],
   email: String
 }, { timestamps: true });
